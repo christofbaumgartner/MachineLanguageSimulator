3b993d147656aa90f9a828b6ed36cba76d55077e
c-heck <git@sonic-force.net>
2020-12-21 00:46:37 +0100

    Update main_read.c

--- main_read.c
+++ main_read.c
@@ -17,6 +17,7 @@ int process_line(char line[]);
 
 int main(int argc, char * argv[]){
 	
+	
 	int i = 0, j;
 	char c;
 	char cmdline_input[MAX_NO_OF_ADDRESSES * MAX_INPUT_LINE_LENGTH];
@@ -35,15 +36,21 @@ int main(int argc, char * argv[]){
 	
 	struct mcode_line mcode[MAX_NO_OF_ADDRESSES];
 	
+	
+		
+	printf("Enter your pseudo machine code. Type '!' when done.\n\n");
+	
 	c = getchar();
 	while (c != '\0' && i < (MAX_INPUT_LINE_LENGTH * MAX_NO_OF_ADDRESSES) -1){
-		if (c != '\0' || c != EOF){
+		if (c != '\0' && c != EOF && c != '!'){
 			cmdline_input[i++] = c;
 			c = getchar();
 		} else {
 			cmdline_input[i++] = '\0';
 		}
 	}
+	
+	printf("Print back input. Marker 2\n");
 	
 	/* DEBUG START: Print back Code*/
 	for (i = 0; i < (MAX_INPUT_LINE_LENGTH * MAX_NO_OF_ADDRESSES) - 1; i++){
@@ -54,14 +61,16 @@ int main(int argc, char * argv[]){
 	/* DEBUG END*/
 	
 	
-	
+	printf("Split line. Marker 3\n");
 	i = 0;	
-	while (cmdline_input[i] != '\n'){
+	while (cmdline_input[i] != '\n' && cmdline_input[i] != '\0'){
 		line[i] = cmdline_input[i];
 		i++;
 	}
 	line[i] = '\0';
-				
+			
+	printf("Print back line. Marker 4\n");
+	
 	/* DEBUG START: Print back line */ 
 	printf("\n");
 	for (j = 0; j < (MAX_INPUT_LINE_LENGTH) - 1; ++j){
@@ -71,19 +80,19 @@ int main(int argc, char * argv[]){
 			continue;
 		}
 	}
-		/* DEBUG END*/
+	/* DEBUG END*/
 		
+	printf("\nEnd. Marker 5\n");
 	
 	
-	if ((strcmp(argv[1], help1) == 0) || (strcmp(argv[1], help2) == 0)){
+	if ((argc == 2 && strcmp(argv[1], "--help") == 0) || (strcmp(argv[1], "-h") == 0)){
 		printf("\n Help:\n\n Pass your pseudo machine code file: %s.exe < yourcode.mc\n\n An example for pseudo code is:\n\n", argv[0]);
 		printf(" P1 INIT \tS2\n P2 INCREMENT \tS2\n P3 JUMP0 \tP7,S1\n P4 ADD \tS2,S2\n P5 DECREMENT \tS1\n P6 JUMP \tP3\n P7 RETURN \tS2\n S1 \t\t2\n S2\n");
 		return 0;
 	}
 	
-	printf("\n Debug: Number of arguments passed: %i", argc);
-
 	return 0;
+	
 }
 
 
